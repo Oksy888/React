92657221111d3286baea1f143105177fff151c7c
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2023-02-19 18:47:18 +0200

    all changes

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './styles/style.scss'
+
+function App() {
+  return (
+    <body>
+      <h1>Some names of programming language</h1>
+      <ul>
+        <p>Python</p>
+        <p>JavaScript</p>
+        <p>Swift</p>
+        <p>JAVA</p>
+        <p>C</p>
+      </ul>
+    </body>
+  )
+}
+
+export default App
---
+++ src/components/Wrapper.js
@@ -0,0 +1,16 @@
+import styles from './Modal.module.scss'
+
+export function Wrapper(props) {
+  const style = {
+    backgroundColor: props.color,
+    width: 250,
+    padding: 20,
+    margin: '20px auto',
+  }
+  return (
+    <div style={style}>
+      {props.children}
+      <div className={styles.close} onClick={props.action}></div>
+    </div>
+  )
+}
