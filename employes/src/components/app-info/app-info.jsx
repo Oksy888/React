37696b2f93abed522c33fa19dbb96a5c0e08b664
Oksy888 <103371949+Oksy888@users.noreply.github.com>
2023-10-11 19:30:39 +0300

    add change of state increase and rise,data-toggle

--- employes/src/components/app-info/app-info.jsx
+++ employes/src/components/app-info/app-info.jsx
@@ -1,11 +1,11 @@
 import './app-info.css'
 
-const AppInfo = () => {
+const AppInfo = ({ countItems, countIncrease }) => {
   return (
     <div className="app-info">
       <h1> Employes accountant</h1>
-      <h2> Total amount of employes:</h2>
-      <h2> Bonus will get:</h2>
+      <h2> Total amount of employes: {countItems}</h2>
+      <h2> Bonus will get: {countIncrease}</h2>
     </div>
   )
 }
--- employes/src/components/employes-add-form/employes-add-form.jsx
+++ employes/src/components/employes-add-form/employes-add-form.jsx
@@ -1,27 +1,65 @@
+import { Component } from 'react'
 import './employees-add-form.css'
 
-const EmployeesAddForm = () => {
-  return (
-    <div className="app-add-form">
-      <h3>Add new employee</h3>
-      <form className="add-form d-flex">
-        <input
-          type="text"
-          className="form-control new-post-label"
-          placeholder="His name?"
-        />
-        <input
-          type="number"
-          className="form-control new-post-label"
-          placeholder="Salary in $?"
-        />
+class EmployeesAddForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      salary: '',
+    }
+  }
 
-        <button type="submit" className="btn btn-outline-light">
-          Add
-        </button>
-      </form>
-    </div>
-  )
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+  onSubmit = (e) => {
+    e.preventDefault()
+    const { name, salary } = this.state
+    const { onAdd } = this.props
+    onAdd(name, salary)
+    this.setState({
+      name: '',
+      salary: '',
+    })
+  }
+  render() {
+    const { name, salary } = this.state
+    const newItem = {
+      name: name,
+      salary: salary,
+    }
+    //console.log(newItem)
+    return (
+      <div className="app-add-form">
+        <h3>Add new employee</h3>
+        <form className="add-form d-flex" onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            className="form-control new-post-label"
+            placeholder="His name?"
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            name="salary"
+            value={salary}
+            className="form-control new-post-label"
+            placeholder="Salary in $?"
+            onChange={this.onValueChange}
+          />
+
+          <button type="submit" className="btn btn-outline-light">
+            Add
+          </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default EmployeesAddForm
